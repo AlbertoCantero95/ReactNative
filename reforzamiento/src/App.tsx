c206b0dce62971de24afeb89b522fbb39ae05772
Alberto Cantero <albertocantero.social@gmail.com>
2023-03-31 13:30:23 +0200

    Se crea el proyecto

---
+++ reforzamiento/src/App.tsx
@@ -0,0 +1,17 @@
+// import { TiposBasicos } from "./typescript/TiposBasicos";
+// import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
+import { Funciones } from './typescript/Funciones';
+
+const App = () => {
+  return (
+    <div className="mt-2">
+      <h1>Introducción a TS - React</h1>
+      <hr/>
+      {/* <TiposBasicos/> */}
+      {/* <ObjetosLiterales/> */}
+      <Funciones/>
+    </div>
+  );
+}
+
+export default App;
